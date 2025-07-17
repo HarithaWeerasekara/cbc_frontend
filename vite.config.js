d0cfe0369d61193e56b8f124339e38293cc5dac3
HarithaWeerasekara <Harithaweerasekara128@gmail.com>
2025-07-17 17:04:26 +0530

    day 19 done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // or use your IP address directly: '192.168.8.139'
+    port: 5173
+  },
   plugins: [react(),  tailwindcss()],
 })
